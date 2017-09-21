bf9099e11a5dc9fcdddbc51c40a29f5756be21b8
Joorren <jorendandois@hotmail.com>
2017-09-21 12:12:05 +0200

    Changed if-statements

--- opdracht-conditional-statements-if/deel2.php
+++ opdracht-conditional-statements-if/deel2.php
@@ -6,22 +6,22 @@ $day = "Not a valid day";
 if($number === 1) {
     $day = "monday";
 }
-else if($number === 2) {
+if($number === 2) {
      $day = "tuesday";
  }
-else if($number === 3) {
+if($number === 3) {
      $day = "wednesday";
  }
-else if($number === 4) {
+if($number === 4) {
      $day = "thursday";
  }
-else if($number === 5) {
+if($number === 5) {
      $day = "friday";
  }
-else if($number === 7) {
+if($number === 7) {
      $day = "saturday";
  }
-else if($number === 7) {
+if($number === 7) {
      $day = "sunday";
  }
 
