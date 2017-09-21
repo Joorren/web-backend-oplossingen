cbc2499f56a3a8da94639f4a769e3e63d1d49e6f
Joorren <jorendandois@hotmail.com>
2017-09-21 20:53:36 +0200

    deel 2 added (ex1 of 3)

---
+++ opdracht-arrays-basis/deel2.php
@@ -0,0 +1,12 @@
+<?php
+
+//Maak een array waarin je de getallen 1, 2, 3, 4, 5 in plaatst
+//Vermenigvuldig alle getallen met elkaar en druk af naar het scherm
+//Druk de oneven getallen af (controle in script, niet zelf selecteren welke je afdrukt)
+//Maak een tweede array waarin je de getallen 5, 4, 3, 2, 1 in plaatst
+//Tel de getallen uit beide arrays met dezelfde index met elkaar op
+
+$numbers = array(1,2,3,4,5);
+
+echo $numbers[0]*$numbers[1]*$numbers[2]*$numbers[3]*$numbers[4];
+
