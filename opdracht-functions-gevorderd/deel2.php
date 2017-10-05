e0f68a06bc2a02960f05db320fa0d66405139591
Joorren <jorendandois@hotmail.com>
2017-10-05 12:28:16 +0200

    fixed + deel 1 added

--- opdracht-functions-gevorderd/deel2.php
+++ opdracht-functions-gevorderd/deel2.php
@@ -1,26 +1,42 @@
 <?php
-
-$money = 100000;
-$percentage = 8;
-$time = 10;
-
-function interestCalculator($money, $time, $percentage) {
-    static $counter=1;
-
-    if ($counter < $time) {
-
-        $interest = floor($money*($percentage/100));
-        $money += $interest;
-
-        echo "The new amount is €" . $money . ", with an interest of €" . $interest . ".<br />";
-
-        $counter++;
-        interestCalculator($money, $time, $percentage);
+$pigHealth = 5;
+$maximumTrows = 8;
+function calculateHit() {
+    global $pigHealth;
+    $hitChance = 40;
+    $hit =  (rand(0,100) <= $hitChance) ? true : false;
+    if ($hit) {
+        $pigHealth--;
+        if ($pigHealth === 1) {
+            $text = "Hit, there is only " . $pigHealth . " pig left.";
+        }
+        else {
+            $text = "Hit, there are only " . $pigHealth . " pigs left.";
+        }
     }
     else {
-        echo "After " . $GLOBALS['time'] . " years with a percentage of " . $percentage .
-            ", you still have " . $money . ".";
+        if ($pigHealth === 1) {
+            $text = "Missed. There is " . $pigHealth . " pig left.";
+        }
+        else {
+            $text = "Missed. There are " . $pigHealth . " pigs left.";
+        }
     }
+    return $text;
 }
-
-interestCalculator($money, $time, $percentage);
+function launchAngryBird() {
+    global $pigHealth, $maximumTrows;
+    static $amount;
+    while ($amount < $maximumTrows) {
+        echo calculateHit() . "<br />";
+        $amount++;
+    }
+    if ($pigHealth < 1) {
+        $text = "You won!";
+    }
+    else {
+        $text = "You lost!";
+    }
+    return $text;
+}
+echo launchAngryBird();
---
+++ opdracht-functions-recursive/deel1.php
@@ -0,0 +1,26 @@
+<?php
+
+$money = 100000;
+$percentage = 8;
+$time = 10;
+
+function interestCalculator($money, $time, $percentage) {
+    static $counter=1;
+
+    if ($counter < $time) {
+
+        $interest = floor($money*($percentage/100));
+        $money += $interest;
+
+        echo "The new amount is €" . $money . ", with an interest of €" . $interest . ".<br />";
+
+        $counter++;
+        interestCalculator($money, $time, $percentage);
+    }
+    else {
+        echo "After " . $GLOBALS['time'] . " years with a percentage of " . $percentage .
+            ", you still have " . $money . ".";
+    }
+}
+
+interestCalculator($money, $time, $percentage);
