bc04a66afe21e19de49f584bccc43b555fd86b58
Joorren <jorendandois@hotmail.com>
2017-10-12 12:42:47 +0200

    removed comments

--- opdracht-date/deel1.php
+++ opdracht-date/deel1.php
@@ -1,11 +1,5 @@
 <?php
 
-//Maak een geldig HTML document
-//Zet deze datum 22u 35m 25sec 21 januari 1904 om naar een timestamp
-//Toon deze timestamp daarna in het volgende formaat: 21 January 1904, 10:35:25 pm
-//
-//int mktime ([ int $hour = date("H") [, int $minute = date("i") [, int $second = date("s")
-// [, int $month = date("n") [, int $day = date("j") [, int $year = date("Y") [, int $is_dst = -1 ]]]]]]] )
 $date = mktime(22, 35, 25, 1, 21, 1904);
 ?>
 
