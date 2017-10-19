78a2a3b10114b225b66118588dbfa685287c3d01
Joorren <jorendandois@hotmail.com>
2017-10-19 10:33:36 +0200

    deel2 added

--- opdracht-cookies/login.php
+++ opdracht-cookies/login.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!isset($_COOKIE["login"]) || $_COOKIE["login"] !== "true") {
+    header("location: deel2.php");
+}
+
 if (isset($_GET["a"]) && $_GET["a"] === "logout" ) {
     setcookie("login", "false");
     header("location: deel1.php");
@@ -13,6 +17,8 @@ if (isset($_GET["a"]) && $_GET["a"] === "logout" ) {
     <title>Logged in</title>
 </head>
 <body>
+
+<p>Hello</p>
 <a href="login.php?a=logout">Log out</a>
 </body>
 </html>
