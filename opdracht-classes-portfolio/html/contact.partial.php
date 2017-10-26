5389a000ae8fcb2588772c85d6e582ffa0ddac10
Joorren <jorendandois@hotmail.com>
2017-10-26 11:20:35 +0200

    uitbreiding 1 added

---
+++ opdracht-classes-portfolio/html/contact.partial.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<body>
+<div class="content">
+    <div class="content-left">
+        <h1>Contact</h1>
+    </div>
+    <div class="content-right">
+        <div class="profile">
+            <img src="http://web-backend.local/img/theo-jansen.jpg" alt="theo jansen" />
+            <h1>Theo Jansen</h1>
+        </div>
+    </div>
+</div>
