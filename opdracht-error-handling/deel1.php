8caba7bd57b6571b760bb60d3af964ba82a81474
Joorren <jorendandois@hotmail.com>
2017-11-24 10:30:58 +0100

    Deel 1 added

---
+++ opdracht-error-handling/deel1.php
@@ -0,0 +1,62 @@
+<?php
+
+
+try {
+    $db = new pdo('mysql:host=localhost;dbname=bieren','root','');
+}
+
+catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+
+$result = $db->query("
+SELECT * FROM bieren AS bi 
+INNER JOIN brouwers AS br 
+ON (bi.brouwernr = br.brouwernr) 
+WHERE bi.naam LIKE 'du%' AND br.brnaam LIKE '%a%'");
+?>
+
+    <head>
+        <style>
+            tr:nth-child(even) {
+                background: #ddd;
+            }
+        </style>
+    </head>
+    <body>
+    <table>
+        <thead>
+        <th>#</th>
+        <th>biernr (PK)</th>
+        <th>naam</th>
+        <th>brouwernr</th>
+        <th>soortnr</th>
+        <th>alcohol</th>
+        <th>brnaam</th>
+        <th>adres</th>
+        <th>postcode</th>
+        <th>gemeente</th>
+        <th>omzet</th>
+        </thead>
+        <tbody>
+<?php
+while($row = $result->fetch()) {
+    static $nr = 1;
+    //echo ($nr % 2) ? '<tr>' : '<tr class="even">';
+    echo '<tr>';
+    echo "<td>$nr</td>";
+    echo "<td>" . $row['biernr'] . "</td>";
+    echo "<td>" . $row['naam'] . "</td>";
+    echo "<td>" . $row['brouwernr'] . "</td>";
+    echo "<td>" . $row['soortnr'] . "</td>";
+    echo "<td>" . $row['alcohol'] . "</td>";
+    echo "<td>" . $row['brnaam'] . "</td>";
+    echo "<td>" . $row['adres'] . "</td>";
+    echo "<td>" . $row['postcode'] . "</td>";
+    echo "<td>" . $row['gemeente'] . "</td>";
+    echo "<td>" . $row['omzet'] . "</td>";
+    echo "</tr>";
+    $nr++;
+}
+echo '</tbody></table>';
