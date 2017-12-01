423fa05b5a50e070ca579a7451687cc03652b17a
Joorren <jorendandois@hotmail.com>
2017-12-01 11:03:02 +0100

    registration form added

--- opdracht-security-login/registratie-form.php
+++ opdracht-security-login/registratie-form.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Joren
- * Date: 1/12/2017
- * Time: 10:36
- */
+
+session_start();
+
+$email = (isset($_SESSION[ 'registration' ][ 'email' ]))?$_SESSION[ 'registration' ][ 'email' ]:'';
+$password = (isset($_SESSION[ 'registration' ][ 'password' ]))?$_SESSION[ 'registration' ][ 'password' ]:'';
+
+?>
+<!DOCTYPE html>
+<html>
+<title>Login Form</title>
+</html>
+<body>
+<h1>Registreren</h1>
+<form action="registratie-process.php" method="post">
+    <label for="email">Email</label><br />
+    <input type="email" id="email" placeholder="email" name="email" value="<?=$email;?>" /><br />
+    <label for="password">Wachtwoord</label><br />
+    <input type="<?=($password!='')?'text':'password';?>" id="password" name="password" placeholder="password" value="<?=$password;?>" />
+    <input type="submit" id="generate" name="generate" value="Genereer Wachtwoord" /><br />
+    <input type="submit" id="submit" value="Registreren" />
+</form>
+</body>
