71b24aa10fb62e87f604a66ec2cf37262393cb4b
Joorren <jorendandois@hotmail.com>
2017-12-01 11:55:00 +0100

    Registration progress added

--- opdracht-security-login/registratie-form.php
+++ opdracht-security-login/registratie-form.php
@@ -12,12 +12,15 @@ $password = (isset($_SESSION[ 'registration' ][ 'password' ]))?$_SESSION[ 'regis
 </html>
 <body>
 <h1>Registreren</h1>
+<?php if (isset($_SESSION[ 'registration' ][ 'error' ])) {
+    echo $_SESSION[ 'registration' ][ 'error' ];
+} ?>
 <form action="registratie-process.php" method="post">
     <label for="email">Email</label><br />
     <input type="email" id="email" placeholder="email" name="email" value="<?=$email;?>" /><br />
     <label for="password">Wachtwoord</label><br />
     <input type="<?=($password!='')?'text':'password';?>" id="password" name="password" placeholder="password" value="<?=$password;?>" />
     <input type="submit" id="generate" name="generate" value="Genereer Wachtwoord" /><br />
-    <input type="submit" id="submit" value="Registreren" />
+    <input type="submit" id="submit" name="submit" value="Registreren" />
 </form>
 </body>
