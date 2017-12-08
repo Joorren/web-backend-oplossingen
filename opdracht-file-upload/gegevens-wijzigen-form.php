c0ba445d2dda7412316a882d9f65085074a71e40
Joorren <jorendandois@hotmail.com>
2017-12-08 10:17:48 +0100

    Added content and profile pic

--- opdracht-file-upload/gegevens-wijzigen-form.php
+++ opdracht-file-upload/gegevens-wijzigen-form.php
@@ -1,7 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Joren
- * Date: 8/12/2017
- * Time: 10:07
- */
+
+session_start();
+
+if (!isset($_COOKIE['login']) || strlen($_COOKIE['login']) <= 0) {
+    $_SESSION['account']['error']      = "U moet eerst inloggen.";
+    header('location: login-form.php');
+    exit();
+}
+
+$accountInfo = explode(",", $_COOKIE['login']);
+
+
+try {
+    $db = new pdo('mysql:host=localhost;dbname=opdracht-file-upload','root','');
+}
+
+catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+$result = $db->query("SELECT * FROM users WHERE email = '$accountInfo[0]' LIMIT 1");
+
+$row  = $result -> fetch();
+
+if (!($row['password'] === $accountInfo[1])) {
+    $_SESSION['account']['error']      = "Error.";
+    setcookie("login","",time()-100);
+    header('location: login-form.php');
+    exit();
+}
+
+$profilePicture = $row['profile_picture'];
+
+?>
+
+<a href="dashboard.php">Terug naar dashboard</a> | Ingelogd als <?= $accountInfo[0];?> | <a href="logout.php">Uitloggen</a>
+<br /><br />
+
+<img src="img/<?=$profilePicture;?>" alt="<?=$accountInfo[0];?>" />
