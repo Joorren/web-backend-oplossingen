4c854253c51b569d2225fbfc21c914b6140ed01d
Joorren <jorendandois@hotmail.com>
2017-12-08 11:31:09 +0100

    PC changing

--- opdracht-file-upload/gegevens-bewerken.php
+++ opdracht-file-upload/gegevens-bewerken.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Joren
- * Date: 8/12/2017
- * Time: 10:23
- */
+
+if (isset($_FILES['profilePicture'])) {
+
+    $dest = "img/";
+
+    switch (substr($_FILES['profilePicture'],-4)) {
+        case ".png":
+        case ".jpg":
+        case ".gif":
+            //upload file
+            move_uploaded_file($_FILES['profilePicture'], $dest.date("YmdHis")."_".$_FILES['profilePicture']);
+            break;
+        default:
+            //error
+
+            break;
+    }
+}
+
+//$file_parts = pathinfo($filename);
+//$file_parts['extension'];
