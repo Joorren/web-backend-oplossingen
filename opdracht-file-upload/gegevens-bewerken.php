44da27f3da714d9838197a487b470d3f985c8990
Joorren <Jorendandois@hotmail.com>
2017-12-10 16:19:11 +0100

    Error handling

--- opdracht-file-upload/gegevens-bewerken.php
+++ opdracht-file-upload/gegevens-bewerken.php
@@ -1,22 +1,41 @@
 <?php
 
+session_start();
+
 if (isset($_FILES['profilePicture'])) {
 
     $dest = "img/";
+    $newFile = $_FILES['profilePicture'];
 
-    switch (substr($_FILES['profilePicture'],-4)) {
-        case ".png":
-        case ".jpg":
-        case ".gif":
-            //upload file
-            move_uploaded_file($_FILES['profilePicture'], $dest.date("YmdHis")."_".$_FILES['profilePicture']);
-            break;
-        default:
-            //error
+    var_dump($_FILES['profilePicture']);
 
-            break;
+    if ($newFile['error'] !== 0) {
+        $_SESSION['upload']['error'] = $newFile['error'];
+        header('url: gegevens-wijzigen-form.php');
+        exit();
     }
+
+//    switch (substr($_FILES['profilePicture'],-4)) {
+//        case ".png":
+//        case ".jpg":
+//        case ".gif":
+//            //upload file
+//            move_uploaded_file($_FILES['profilePicture'], $dest.date("YmdHis")."_".$_FILES['profilePicture']);
+//            break;
+//        default:
+//            //error
+//
+//            break;
+//    }
 }
 
 //$file_parts = pathinfo($filename);
 //$file_parts['extension'];
+
+
+//["name"] ["type"] ["tmp_name"] ["error"] ["size"]
+
+// ["type"]=> string(10) "image/jpeg"
+// ["type"]=> string(9) "image/png"
+// ["type"]=> string(9) "image/bmp"
+// ["type"]=> string(9) "image/gif"
