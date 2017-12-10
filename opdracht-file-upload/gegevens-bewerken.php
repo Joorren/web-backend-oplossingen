cb1cb5fc6f04897308e3b6feb7914908d651e7d4
Joorren <Jorendandois@hotmail.com>
2017-12-10 16:46:59 +0100

    Add error message

--- opdracht-file-upload/gegevens-bewerken.php
+++ opdracht-file-upload/gegevens-bewerken.php
@@ -7,11 +7,40 @@ if (isset($_FILES['profilePicture'])) {
     $dest = "img/";
     $newFile = $_FILES['profilePicture'];
 
-    var_dump($_FILES['profilePicture']);
+    if ($newFile["error"] !== 0) {
+        $errMessage = "";
+        switch ($newFile['error']) {
+            case 1:
+            case 2:
+                $errMessage="Bestand is te groot";
+                break;
+            case 3:
+                $errMessage="Bestand is recentelijk geupload";
+                break;
+            case 4:
+                $errMessage="Geen bestand toegevoegd";
+                break;
+            case 6:
+                $errMessage="Geen tijdelijke map gevonden";
+                break;
+            case 7:
+                $errMessage="Mislukt om bestand te schrijven";
+                break;
+            case 8:
+                $errMessage="Php extentie stopte de upload";
+                break;
+            default:
+                $errMessage="Error";
+                break;
+        }
 
-    if ($newFile['error'] !== 0) {
-        $_SESSION['upload']['error'] = $newFile['error'];
-        header('url: gegevens-wijzigen-form.php');
+        $_SESSION['upload']['error'] = $errMessage;
+        header('location: gegevens-wijzigen-form.php');
+        exit();
+    }
+    if ($newFile['size'] > 2097152) { // if bigger than 2mb (2*1024*1024)
+        $_SESSION['upload']['error'] = "Bestand is te groot";
+        header('location: gegevens-wijzigen-form.php');
         exit();
     }
 
@@ -37,5 +66,7 @@ if (isset($_FILES['profilePicture'])) {
 
 // ["type"]=> string(10) "image/jpeg"
 // ["type"]=> string(9) "image/png"
-// ["type"]=> string(9) "image/bmp"
 // ["type"]=> string(9) "image/gif"
+
+
+// $dest.date("YmdHis")
