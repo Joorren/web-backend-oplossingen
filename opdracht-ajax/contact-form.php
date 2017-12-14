3d1192da30e53a34f159fc07ffe806fd5689c876
Joorren <Jorendandois@hotmail.com>
2017-12-14 23:49:51 +0100

    form added

--- opdracht-ajax/contact-form.php
+++ opdracht-ajax/contact-form.php
@@ -1,1 +1,13 @@
 <?php
+
+?>
+
+<h1>Contacteer ons</h1>
+
+<form>
+    <label for="email">E-mailadres</label><br />
+    <input type="email" name="email" id="email" ><br /><br />
+    <label for="message">Boodschap</label><br />
+    <input type="text" name="message" id="message"><br /><br />
+<input type="checkbox" name="copy" id="copy"><label for="copy">Stuur een kopie naar mezelf</label><br /><br />
+<input type="submit" value="Submit" name="submit" id="submit">
