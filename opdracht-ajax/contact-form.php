ddb3b2c0e787ad5324508e184e46f346c0f76072
Joorren <Jorendandois@hotmail.com>
2017-12-15 00:00:40 +0100

    Session added

--- opdracht-ajax/contact-form.php
+++ opdracht-ajax/contact-form.php
@@ -1,13 +1,23 @@
 <?php
 
+session_start();
+
+$errorMessage = (isset($_SESSION['contact']['error']) && $_SESSION['contact']['error'])? $_SESSION['contact']['error'] : '';
+$email = (isset($_SESSION['contact']['email']) && $_SESSION['contact']['email'])? $_SESSION['contact']['email'] : '';
+$message = (isset($_SESSION['contact']['message']) && $_SESSION['contact']['message'])? $_SESSION['contact']['message'] : '';
+$copy = (isset($_SESSION['contact']['copy']) && $_SESSION['contact']['copy'])? 'selected' : '';
+
+
 ?>
 
 <h1>Contacteer ons</h1>
 
+<p><?=$errorMessage;?></p>
+
 <form method="post" action="contact.php">
     <label for="email">E-mailadres</label><br />
-    <input type="email" name="email" id="email" ><br /><br />
+    <input type="email" name="email" id="email" value="<?=$email;?>"><br /><br />
     <label for="message">Boodschap</label><br />
-    <input type="text" name="message" id="message"><br /><br />
-<input type="checkbox" name="copy" id="copy"><label for="copy">Stuur een kopie naar mezelf</label><br /><br />
+    <input type="text" name="message" id="message" value="<?=$message;?>"><br /><br />
+<input type="checkbox" name="copy" id="copy" <?=$copy;?>><label for="copy">Stuur een kopie naar mezelf</label><br /><br />
 <input type="submit" value="Submit" name="submit" id="submit">
