55cd432f8482e46880d38835188e36bea106cf3b
Joorren <jorendandois@hotmail.com>
2017-12-15 11:36:11 +0100

    api added

--- opdracht-ajax/contact-API.php
+++ opdracht-ajax/contact-API.php
@@ -1,1 +1,52 @@
 <?php
+
+session_start();
+
+
+$ajaxMessage['type'] = "";
+
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    if (isset($_POST['email']) && isset($_POST['message'])) {
+        $ajaxMessage['type'] = 'Success';
+    }
+    else {
+        $ajaxMessage['type'] = 'Failed';
+    }
+}
+
+
+$email = (isset($_POST['email']))? $_POST['email'] : '';
+$_SESSION['contact']['email'] = $email;
+$message = (isset($_POST['message']))? $_POST['message'] : '';
+$_SESSION['contact']['message'] = $message;
+$copy = (isset($_POST['copy']))? $_POST['copy'] : '';
+$_SESSION['contact']['copy'] = $copy;
+$date = date('Y-m-d H:i:s');
+
+$admin = 'Jorendandois@hotmail.com';
+$title= "[TEST] Email";
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+try {
+    $db = new pdo('mysql:host=localhost;dbname=opdracht-ajax','root','');
+}
+catch (Exception $e) {
+    echo $e->getMessage();
+}
+$sql = "INSERT INTO `opdracht-ajax`.contact_messages (id, email, message, time_sent) VALUES (0,'$email', '$message', '$date')";
+$db->query($sql);
+
+//mail($admin,$title,$message,$headers);
+
+//if ($copy==='copy') {
+//    mail($email,$title,$message,$headers);
+//}
+
+unset($_SESSION['contact']["email"]);
+unset($_SESSION['contact']["message"]);
+unset($_SESSION['contact']["copy"]);
+
+
+echo json_encode($ajaxMessage);
--- opdracht-ajax/contact-form.php
+++ opdracht-ajax/contact-form.php
@@ -6,6 +6,7 @@ $errorMessage = (isset($_SESSION['contact']['error']) && $_SESSION['contact']['e
 $email = (isset($_SESSION['contact']['email']) && $_SESSION['contact']['email'])? $_SESSION['contact']['email'] : '';
 $message = (isset($_SESSION['contact']['message']) && $_SESSION['contact']['message'])? $_SESSION['contact']['message'] : '';
 $copy = (isset($_SESSION['contact']['copy']) && $_SESSION['contact']['copy'] === 'copy')? 'checked' : '';
+unset($_SESSION['contact']);
 
 
 ?>
@@ -13,14 +14,33 @@ $copy = (isset($_SESSION['contact']['copy']) && $_SESSION['contact']['copy'] ===
 <h1>Contacteer ons</h1>
 
 <p><?=$errorMessage;?></p>
-
-<form method="post" action="contact.php">
-    <label for="email">E-mailadres</label><br />
-    <input type="email" name="email" id="email" value="<?=$email;?>"><br /><br />
-    <label for="message">Boodschap</label><br />
-    <input type="text" name="message" id="message" value="<?=$message;?>"><br /><br />
-<input type="checkbox" name="copy" id="copy" value="copy" <?=$copy;?>><label for="copy">Stuur een kopie naar mezelf</label><br /><br />
-<input type="submit" value="Submit" name="submit" id="submit">
-</form>
+<div id="form-container">
+    <form method="post" action="contact.php" id="form">
+        <label for="email">E-mailadres</label><br />
+        <input type="email" name="email" id="email" value="<?=$email;?>"><br /><br />
+        <label for="message">Boodschap</label><br />
+        <input type="text" name="message" id="message" value="<?=$message;?>"><br /><br />
+        <input type="checkbox" name="copy" id="copy" value="copy" <?=$copy;?>><label for="copy">Stuur een kopie naar mezelf</label><br /><br />
+        <input type="submit" value="Submit" name="submit" id="submit">
+    </form>
+</div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+    $(function()
+    {
+        $('#form').submit(function() {
+            var formData	=	$('#form').serialize();
+            $.ajax({
+                type: 'POST',
+                url: 'contact-API.php',
+                data: formData,
+                success: function (data) {
+                    data = JSON.parse(data);
+                    console.log(data);
+                }
+            });
+            return false;
+        });
+    });
+</script>
