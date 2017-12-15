e62db3c1f52c25e774da9febe99ffbc923f6ab84
Joorren <jorendandois@hotmail.com>
2017-12-15 11:55:32 +0100

    regex working

--- opdracht-regular-expressions/deel1.php
+++ opdracht-regular-expressions/deel1.php
@@ -1,13 +1,17 @@
 <?php
-
+if (isset($_POST['submit'])) {
+    $regex = $_POST['regex'];
+    $string = $_POST['string'];
+    $newString = preg_replace('/' . $regex . '/',"#",$string);
+}
 ?>
 
 <h1>RegEx Tester</h1>
 
 <form method="post">
     <label for="regex">Regular Expression</label><br />
-    <input type="text" id="regex" name="regex"><br /><br />
+    <input type="text" id="regex" name="regex" value="<?=$regex;?>"><br /><br />
     <label for="string">String</label><br />
-    <textarea id="string" name="string"></textarea><br /><br />
+    <textarea id="string" name="string"><?=$newString;?></textarea><br /><br />
     <input type="submit" value="Submit" id="submit" name="submit"/>
 </form>
