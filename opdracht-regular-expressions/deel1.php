331db89f3941eba1af417e169848de60eadddc32
Joorren <jorendandois@hotmail.com>
2017-12-15 11:48:56 +0100

    form added

---
+++ opdracht-regular-expressions/deel1.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<h1>RegEx Tester</h1>
+
+<form method="post">
+    <label for="regex">Regular Expression</label><br />
+    <input type="text" id="regex" name="regex"><br /><br />
+    <label for="string">String</label><br />
+    <textarea id="string" name="string"></textarea><br /><br />
+    <input type="submit" value="Submit" id="submit" name="submit"/>
+</form>
